c97e2c401f68aa29ef5b1900fa7cde9a003d2bfe
Renee Dubuc <radubuc5@gmail.com>
2021-01-28 10:49:36 -0500

    First commit

---
+++ mvnProject/src/main/java/com/reneedubuc/mvnProject/App.java
@@ -0,0 +1,13 @@
+package com.reneedubuc.mvnProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
